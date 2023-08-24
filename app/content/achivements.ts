a853d2bf69d1199fbd738586eafb44c2b80ad395
Alan Cortez <acortez1337@csu.fullerton.edu>
2023-08-23 18:35:55 -0700

    update text in achievements section #0004

--- app/content/achivements.ts
+++ app/content/achivements.ts
@@ -6,6 +6,7 @@ export interface awardData {
   blurb: string;
   link?: string;
   img: string;
+  imgAlt: string;
 }
 
 export const awardsData: awardData[] = [
@@ -18,15 +19,17 @@ export const awardsData: awardData[] = [
       "CSUF's inaugural \"Engineering Social Justice: A Design Challenge\" has students addressing real-world social issues and Bag/Get's first appearance to address food insecurity.",
     link: "https://news.fullerton.edu/2023/03/engineering-computing-students-pitch-solutions-to-social-justice-issues/",
     img: "/esj.webp",
+    imgAlt: "Two members of team Bag/Get posing their first place award on a stage with the panel of coordinators."
   },
   {
-    eventName: "SCAR Day",
+    eventName: "Student Research (SCAR) Day",
     location: "CSU, Fullerton",
     date: "4/17/2023",
-    placement: "1st place",
+    placement: "1st place category",
     blurb:
       "CSUF's SCAR Day: Students showcase research, creativity, and scholarship, fostering a community celebration of accomplishments across all disciplines.",
     img: "/scarday.webp",
+    imgAlt: "Bag/Get team presenting their poster-board covering their research and solution at the time to reduce food insecurity."
   },
   {
     eventName: "Sunstone",
@@ -34,8 +37,9 @@ export const awardsData: awardData[] = [
     date: "5/5/2023",
     placement: "3rd place",
     blurb:
-      "18 CSU campuses competed in the first CSU-wide startup launch at San José State University on May 5, 2023.",
+      "18 California State University campuses competed in the first CSU-wide startup launch at San José State University on May 5, 2023.",
     link: "https://csufentrepreneurship.com/csuf-students-make-social-impact-in-state-entrepreneurship-competition/",
     img: "/sj.webp",
+    imgAlt: "Two members of team Bag/Get showing their $5,000 check after winning 3rd place in Sunstone with the panel of judges."
   },
 ];
