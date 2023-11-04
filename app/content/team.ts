6f0195eec154c5cdb2a6c0d997c82a5fe5185e3a
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2023-11-04 11:20:25 -0700

    update headshot pictures and css alignment

--- app/content/team.ts
+++ app/content/team.ts
@@ -4,20 +4,20 @@ export const teamData = [
     title: "Product Lead & Backend Developer",
     email: "rohan@bagget.io",
     linkedIn: "rohankunchala",
-    badgeImg: "/rohan.webp",
+    badgeImg: "/rohan-hs.webp",
   },
   {
     name: "Alan Cortez",
     title: "UI/UX Frontend Developer",
     email: "alan@bagget.io",
     linkedIn: "alancortez1337",
-    badgeImg: "/alan.webp",
+    badgeImg: "/alan-hs.webp",
   },
   {
     name: "Cesar Rojas",
     title: "Financial & Outreach Lead",
     email: "cesar@bagget.io",
     linkedIn: "cesar-rojas21",
-    badgeImg: "/cesar.webp",
+    badgeImg: "/cesar-hs.webp",
   },
 ];
