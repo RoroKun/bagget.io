ee38f0f7f3e3e8919a920466bcdaf1c1632a0cd9
Alan <cortez.alan060@gmail.com>
2024-01-17 15:55:35 -0800

    Make new Stakeholder data

--- app/content/stakeholders.ts
+++ app/content/stakeholders.ts
@@ -21,3 +21,21 @@ export const stakeholderData = [
     iconName: "People",
   },
 ];
+// TODO: RENAME 
+export const steps = [
+  {
+    title: "Food Banks",
+    description:
+      "These are wholesale distributors who serve up to 60,000 pantries nationwide.",
+  },
+  {
+    title: "Food Pantries",
+    description:
+      "The neighborhood sites with a selection of foods for locals to utilize.",
+  },
+  {
+    title: "The Community",
+    description:
+      "Your next door neighbor, who fights to make ends meet and feed their family.",
+  },
+];
