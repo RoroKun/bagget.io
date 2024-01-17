1326f56431c50958837836b9279f97c8e9e69797
Alan <cortez.alan060@gmail.com>
2024-01-17 15:56:04 -0800

    revamp stakeholder section

--- app/sections/stakeholders.tsx
+++ app/sections/stakeholders.tsx
@@ -14,74 +14,57 @@ import {
   IconProps,
   useColorModeValue,
   Step,
-  StepDescription,
-  StepIcon,
   StepIndicator,
-  StepNumber,
   StepSeparator,
   StepStatus,
-  StepTitle,
   Stepper,
-  useSteps,
 } from "@chakra-ui/react";
+import { steps } from "../content/stakeholders";
 
-const steps = [
-  {
-    title: "Food Banks",
-    description:
-      "These are wholesale distributors who serve up to 60,000 pantries nationwide.",
-  },
-  {
-    title: "Food Pantries",
-    description:
-      "The neighborhood sites with a selection of foods for locals to utilize.",
-  },
-  {
-    title: "The Community",
-    description:
-      "Your next door neighbor, who fights to make ends meet and feed their family.",
-  },
-];
+
 
 export default function Stakeholders() {
   return (
-    <Container maxW={"7xl"}>
-      <Title />
-      <Flex
+    <Container maxW={"7xl"} >
+      <Flex justifyContent={'center'} alignItems={'center'} direction={"column"}>
+        <Title />
+        <Flex
           flex={1}
-          justify={'center'}
+          justify={'space-between'}
           align={'center'}
           position={'relative'}
-          w={'full'}>
-          <InfoBlurb />
-          <Blob
-            w={'150%'}
-            h={'150%'}
-            position={'absolute'}
-            top={'-20%'}
-            left={0}
-            zIndex={-1}
-            color={useColorModeValue('green.50', 'green.400')}
-          />
-          <Box
-            position={'relative'}
-            height={'300px'}
-            rounded={'2xl'}
-            boxShadow={'2xl'}
-            width={'full'}
-            overflow={'hidden'}>
-            {/* <Image
-              alt={'Hero Image'}
-              fit={'cover'}
-              align={'center'}
-              w={'100%'}
-              h={'100%'}
-              src={
-                'https://images.unsplash.com/photo-1599059813005-11265ba4b4ce?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-              }
-            /> */}
-          </Box>
-        </Flex>
+          w={'full'}
+          gap={'10%'}
+          flexWrap={['wrap', 'wrap', 'nowrap']}
+        >
+            <InfoBlurb />
+            <Blob
+              w={'150%'}
+              h={'150%'}
+              position={'absolute'}
+              top={'-20%'}
+              left={0}
+              zIndex={-1}
+              color={useColorModeValue('green.50', 'green.400')}
+            />
+            <Box
+              position={'relative'}
+              height={'full'}
+              width={'full'}
+              overflow={'hidden'}
+              marginTop={["20%", "10%", "0%"]}
+            >
+              <Image
+                alt={'Hero Image'}
+                fit={'cover'}
+                align={'center'}
+                w={'100%'}
+                h={'100%'}
+                src={"/stakeholder-img.webp"}
+              />
+            </Box>
+          </Flex>
+      </Flex>
     </Container>
   );
 }
@@ -101,7 +84,7 @@ function Title() {
           fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
         >
           <Text as={"span"} color={"green.400"}>
-            But to understand the{" "}
+            There are{" "}
           </Text>
           <Text
             as={"span"}
@@ -117,27 +100,10 @@ function Title() {
               zIndex: -1,
             }}
           >
-            problem,
+            THREE
           </Text>
-          <br />
           <Text as={"span"} color={"green.400"}>
-            You need to understand the{" "}
-          </Text>
-          <Text
-            as={"span"}
-            position={"relative"}
-            _after={{
-              content: "''",
-              width: "full",
-              height: "30%",
-              position: "absolute",
-              bottom: 1,
-              left: 0,
-              bg: "green.400",
-              zIndex: -1,
-            }}
-          >
-            people...
+            {" "}impacted parties.
           </Text>
         </Heading>
       </Stack>
@@ -146,12 +112,13 @@ function Title() {
 }
 
 function InfoBlurb() {
+
   return (
     <Stepper
       size="lg"
       index={3}
       orientation="vertical"
-      height="400px"
+      height={["400px", "350px", "500px"]}
       colorScheme="green"
     >
       {steps.map((step, index) => (
@@ -160,18 +127,14 @@ function InfoBlurb() {
             <StepStatus complete={<></>} incomplete={<></>} active={<></>} />
           </StepIndicator>
 
-          <Box flexShrink="0">
-            <StepTitle>
-              <Heading
-                fontWeight={500}
-                fontSize={{ base: "lg", sm: "xl", lg: "3xl" }}
-              >
-                {step.title}
-              </Heading>
-            </StepTitle>
-            <StepDescription>
+          <Box flexShrink="0" width={'90%'}>
+                <Heading
+                  fontWeight={600}
+                  fontSize={{ base: "lg", sm: "xl", lg: "3xl" }}
+                >
+                  {step.title}
+                </Heading>
               <Text fontSize={{ base: "md", sm: "lg", lg: "xl" }}>{step.description}</Text>
-            </StepDescription>
           </Box>
 
           <StepSeparator />
@@ -185,14 +148,14 @@ const Blob = (props: IconProps) => {
     return (
       <Icon
         width={'100%'}
-        viewBox="0 0 578 440"
+        viewBox="0 0 755 588"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         {...props}>
         <path
           fillRule="evenodd"
           clipRule="evenodd"
-          d="M239.184 439.443c-55.13-5.419-110.241-21.365-151.074-58.767C42.307 338.722-7.478 282.729.938 221.217c8.433-61.644 78.896-91.048 126.871-130.712 34.337-28.388 70.198-51.348 112.004-66.78C282.34 8.024 325.382-3.369 370.518.904c54.019 5.115 112.774 10.886 150.881 49.482 39.916 40.427 49.421 100.753 53.385 157.402 4.13 59.015 11.255 128.44-30.444 170.44-41.383 41.683-111.6 19.106-169.213 30.663-46.68 9.364-88.56 35.21-135.943 30.551z"
+          d="M62.4997 111C153.874 14.9541 269.09 149.681 401 136.5C535.99 123.012 687.771 -86.1759 735 41C766.42 125.609 639.615 167.031 651.5 256.5C661.589 332.457 754.379 346.876 754.5 423.5C754.705 554.052 580.073 617.112 456.5 575C378.34 548.364 385.686 454.123 309 423.5C228.891 391.51 162.957 480.054 88.4997 436.5C-21.5724 372.114 -25.3958 203.39 62.4997 111Z"
           fill="currentColor"
         />
       </Icon>
