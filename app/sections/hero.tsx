45e461c541404151bb8b413464e49f30a379145d
Alan Cortez <acortez1337@csu.fullerton.edu>
2024-01-25 11:08:03 -0800

    Add comments

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -26,6 +26,7 @@ export default function Hero() {
     <Container
     maxW={'100%'}
     height={'500px'}
+    // TODO: clean these comments up
     // backgroundImage={
     //   'url(https://images.unsplash.com/photo-1599059813005-11265ba4b4ce?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'
     // }
@@ -84,9 +85,9 @@ export default function Hero() {
             </Text>
           </Stack>
           
-          <Button bgColor={'green.300'} colorScheme="green" variant='solid' color={'white'} fontSize={{ base: '1xl', sm: '2xl', lg: '4xl' }} padding={"30px"}>
+          {/* <Button bgColor={'green.300'} colorScheme="green" variant='solid' color={'white'} fontSize={{ base: '1xl', sm: '2xl', lg: '4xl' }} padding={"30px"}>
             Let's understand why...
-          </Button>
+          </Button> */}
           
         </Stack>
       </Container>
