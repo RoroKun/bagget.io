11abc4587453f93cda8cac7c88303c3ba24f76a6
Alan <cortez.alan060@gmail.com>
2024-01-28 23:02:38 -0800

    add image and styles to new challenge section

--- app/sections/challenges.tsx
+++ app/sections/challenges.tsx
@@ -1,23 +1,15 @@
 "use client"
 
 import {
-    Accordion,
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    AccordionIcon,
     Box,
     Container,
     Text,
     Card,
-    Flex,
     Heading,
     Image,
     VStack,
     Stack,
   } from '@chakra-ui/react'
-
-import { useState } from 'react'
 
 interface challenge {
     title: string;
@@ -33,66 +25,97 @@ const challengesData: challenge[] = [
         accordianTitle: "struggle to...",
         blurb: "...collect data from & communicate with all partnered pantries. Instead of having a streamlined way of maintaining regular & comprehensive communication with their partner agencies, many food banks still rely on personnel to handle relationships with dozens of such pantries each - a strenuous, and inefficient process.",
         image: "/alan-hs.webp",
-        bgColor: 'green.200'
+        bgColor: 'green.100'
     },
     {
         title: "Food Pantries",
         accordianTitle: "are faced with...",
         blurb: "redundant supplies - leading to wastage - whilst also not stocking the items their community wants. Despite evidence that the client-choice model provides a more dignifying experience for the community whilst generating less food waste, pantries often cannot adopt this model due to a dearth of quality software.",
         image: "/cesar-hs.webp",
-        bgColor: 'green.300'
+        bgColor: 'green.200'
     },
     {
         title: "The Community",
         accordianTitle: "feels frustrated with...",
         blurb: "...the difficulty to find info, being unable to schedule appointments in advance, and having no way of receiving updates. In the end, they turn away due to the substandard experience they face.",
         image: "/rohan-hs.webp",
-        bgColor: 'green.400'
+        bgColor: 'green.100'
     }
 ]
 
 
 export default function Challenges() {
-    const [challenge, setChallenge] = useState<challenge>(challengesData[0])
 
     return (
-        <Container maxW={"7xl"}>
-            <VStack spacing='24px' width={"full"}>
-            <Heading
-                lineHeight={1.1}
-                fontWeight={600}
-                fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
-            >
-                <Text as={"span"} color={"green.900"}>
-                    However, under the surface...
-                </Text>
-            </Heading>
-                {challengesData.map((challenge, i) => {
-                    return (
-                        <Card 
-                            width={"full"} 
-                            bgColor={challenge.bgColor} 
-                            height={['200px', '400px']}
-                            key={i}
-                            borderRadius={'80px'}
-                        >
-                            <Stack direction={['column', 'row']} justifyContent={'space-around'} alignItems={'center'} height={'full'}>
-                                <VStack maxWidth={'450px'} alignItems={'self-start'}>
-                                    <Heading>
-                                        {challenge.title}  {challenge.accordianTitle}
-                                    </Heading>
-                                    <Text>
-                                        {challenge.blurb}
-                                    </Text>
-                                </VStack>
-                                <Text>
-                                    asdfasdf
-                                </Text>
-                            </Stack>
-                        </Card>
-                    )
-                })}
-            </VStack>
+        <Container 
+            maxW={"full"} 
+            bg={'green.50'}
+            paddingBottom={'5%'}
+        >
+            <Container maxW={"7xl"} >
+                <VStack spacing='24px' width={"full"}>
+                <Heading
+                    lineHeight={1.1}
+                    fontWeight={600}
+                    fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
+                >
+                    <Text as={"span"} color={"green.900"}>
+                        However, under the surface...
+                    </Text>
+                </Heading>
+                    {challengesData.map((challenge, i) => {
+                        return (
+                            <Card 
+                                width={"full"} 
+                                bgColor={challenge.bgColor} 
+                                height={['200px', '400px']}
+                                key={i}
+                                borderRadius={'80px'}
+                            >
+                                <Stack direction={['column', 'row']} justifyContent={'space-around'} alignItems={'center'} height={'full'}>
+                                    <VStack maxWidth={'450px'} alignItems={'self-start'}>
+                                        <Heading 
+                                            lineHeight={1.1}
+                                        >
+                                            <Text as={"span"} color={"green.800"}>
+                                            {challenge.title} {" "}
+                                            </Text>
+                                            <Text as={"span"} color={"green.800"}>
+                                            {challenge.accordianTitle}
+                                            </Text>
+                                        </Heading>
+                                        <Text 
+                                            as={"span"} 
+                                            color={"green.800"} 
+                                            fontSize={"xl"}
+                                            fontWeight={400}
+                                        >
+                                            {challenge.blurb}
+                                        </Text>
+                                    </VStack>
+                                    <Box
+                                        position={'relative'}
+                                        height={'350px'}
+                                        width={'350px'}
+                                        overflow={'hidden'}
+                                        // marginTop={["20%", "10%", "0%"]}
+                                    >
+                                        <Image
+                                        alt={`${challenge.title} picture`}
+                                        fit={'cover'}
+                                        align={'center'}
+                                        borderRadius={'20px'}
+                                        w={'100%'}
+                                        h={'100%'}
+                                        src={challenge.image}
+                                        />
+                                    </Box>
+                                </Stack>
+                            </Card>
+                        )
+                    })}
+                </VStack>
+            </Container>
         </Container>
     )
 }
