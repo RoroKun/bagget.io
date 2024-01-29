0651e1e2a51d58843c9db4dfc294779fa90e371b
Alan Cortez <acortez1337@csu.fullerton.edu>
2024-01-28 22:09:45 -0800

    minor addition to challenges ui

--- app/sections/challenges.tsx
+++ app/sections/challenges.tsx
@@ -8,9 +8,13 @@ import {
     AccordionIcon,
     Box,
     Container,
+    Text,
+    Card,
     Flex,
     Heading,
-    Image
+    Image,
+    VStack,
+    Stack,
   } from '@chakra-ui/react'
 
 import { useState } from 'react'
@@ -20,6 +24,7 @@ interface challenge {
     accordianTitle: string;
     blurb: string;
     image: string;
+    bgColor: string;
 }
 
 const challengesData: challenge[] = [
@@ -27,19 +32,22 @@ const challengesData: challenge[] = [
         title: "Food Banks",
         accordianTitle: "struggle to...",
         blurb: "...collect data from & communicate with all partnered pantries. Instead of having a streamlined way of maintaining regular & comprehensive communication with their partner agencies, many food banks still rely on personnel to handle relationships with dozens of such pantries each - a strenuous, and inefficient process.",
-        image: "/alan-hs.webp"
+        image: "/alan-hs.webp",
+        bgColor: 'green.200'
     },
     {
         title: "Food Pantries",
         accordianTitle: "are faced with...",
         blurb: "redundant supplies - leading to wastage - whilst also not stocking the items their community wants. Despite evidence that the client-choice model provides a more dignifying experience for the community whilst generating less food waste, pantries often cannot adopt this model due to a dearth of quality software.",
-        image: "/cesar-hs.webp"
+        image: "/cesar-hs.webp",
+        bgColor: 'green.300'
     },
     {
         title: "The Community",
         accordianTitle: "feels frustrated with...",
         blurb: "...the difficulty to find info, being unable to schedule appointments in advance, and having no way of receiving updates. In the end, they turn away due to the substandard experience they face.",
-        image: "/rohan-hs.webp"
+        image: "/rohan-hs.webp",
+        bgColor: 'green.400'
     }
 ]
 
@@ -49,67 +57,42 @@ export default function Challenges() {
 
     return (
         <Container maxW={"7xl"}>
-            <Flex justifyContent={'center'} alignItems={'center'} direction={"column"} gap={'40px'}>
-                
-            <Box
-                position={'relative'}
-                height={'full'}
-                width={'full'}
-                overflow={'hidden'}
-                marginTop={["20%", "10%", "0%"]}
-                >
-                <Image
-                    alt={`${challenge.title} backsplash image`}
-                    fit={'cover'}
-                    align={'center'}
-                    w={'100%'}
-                    h={'350px'}
-                    position={'relative'}
-                    zIndex={-1}
-                    src={challenge.image}
-                />
-                <Heading
-                    lineHeight={1.1}
-                    fontWeight={600}
-                    fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
-                    position={'absolute'}
-                    zIndex={1}
-                    top="50%"
-                    left="50%"
-                    transform="translate(-50%, -50%)"
-                    textAlign="center"
-                    color="white"  // Set text color as needed
-                >
-                    However, the {challenge.title}'s challenges are...
-                </Heading>
-                </Box>
-                <Accordion defaultIndex={[0]}>
-
-                    {challengesData.map((challenge, id) => {
-                        return (
-                            <AccordionItem key={id}>
-                                <h2 onClick={() => setChallenge(challenge)}>
-                                    <AccordionButton>
-                                        <Box 
-                                            as="span" 
-                                            flex='1' 
-                                            textAlign='left' 
-                                            fontWeight={600}
-                                            fontSize={{ base: "lg", sm: "xl", lg: "3xl" }}
-                                        >
-                                            {challenge.title} {challenge.accordianTitle}
-                                        </Box>
-                                        <AccordionIcon />
-                                    </AccordionButton>
-                                </h2>
-                                <AccordionPanel pb={4} fontSize={{ base: "md", sm: "lg", lg: "xl" }}>
-                                {challenge.blurb}
-                                </AccordionPanel>
-                            </AccordionItem>
-                        )
-                    })}
-                </Accordion>
-            </Flex>
+            <VStack spacing='24px' width={"full"}>
+            <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
+            >
+                <Text as={"span"} color={"green.900"}>
+                    However, under the surface...
+                </Text>
+            </Heading>
+                {challengesData.map((challenge, i) => {
+                    return (
+                        <Card 
+                            width={"full"} 
+                            bgColor={challenge.bgColor} 
+                            height={['200px', '400px']}
+                            key={i}
+                            borderRadius={'80px'}
+                        >
+                            <Stack direction={['column', 'row']} justifyContent={'space-around'} alignItems={'center'} height={'full'}>
+                                <VStack maxWidth={'450px'} alignItems={'self-start'}>
+                                    <Heading>
+                                        {challenge.title}  {challenge.accordianTitle}
+                                    </Heading>
+                                    <Text>
+                                        {challenge.blurb}
+                                    </Text>
+                                </VStack>
+                                <Text>
+                                    asdfasdf
+                                </Text>
+                            </Stack>
+                        </Card>
+                    )
+                })}
+            </VStack>
         </Container>
     )
 }
