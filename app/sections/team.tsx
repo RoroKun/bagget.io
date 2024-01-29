3e1d3ff19c7b9dfeb486e5824e51d15757caffd5
Alan <cortez.alan060@gmail.com>
2024-01-28 23:03:19 -0800

    add section for new team section

---
+++ app/sections/team.tsx
@@ -0,0 +1,29 @@
+import {
+    Box,
+    Container,
+    Text,
+    Card,
+    Heading,
+    Image,
+    VStack,
+    Stack,
+  } from '@chakra-ui/react'
+
+export default function Team() {
+
+    return (
+        <Container 
+            maxW={"full"} 
+            bg={'green.800'}
+            paddingBottom={'5%'}
+        >
+            <Container maxW={"7xl"} >
+                <VStack spacing='24px' width={"full"}>
+                <Text>
+                    asdfasd
+                </Text>
+                </VStack>
+            </Container>
+        </Container>
+    )
+}
