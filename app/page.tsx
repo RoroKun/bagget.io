0e18ecd9583a1fdbdecbd9660e10692d448ae73a
Alan Cortez <acortez1337@csu.fullerton.edu>
2024-02-04 22:45:18 -0800

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,19 @@ import Hero from "./sections/hero";
 import Stakeholders from "./sections/stakeholders";
 import Challenges from "./sections/challenges";
 import Team from "./sections/team";
+import Awards from "./sections/awards";
 
 export default function Home() {
   return (
     <>
       <main>
         <Hero />
+        <Awards />
         <Stakeholders/>
         <Challenges />
         <Team />
         
-        
       </main>
-      <footer>
-        <p className="sub-text primary-text">© 2023 All rights reserved.</p>
-      </footer>
     </>
   );
 }
