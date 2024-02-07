2208a78284105a836417ec5f9c7cd3025ff59637
Alan Cortez <acortez1337@csu.fullerton.edu>
2024-02-06 21:55:33 -0800

    create footer section

---
+++ app/components/footer.tsx
@@ -0,0 +1,27 @@
+import { Container, Flex, Text } from "@chakra-ui/react";
+
+export default function Footer() {
+    return(
+        <Container 
+            as={'footer'}
+            maxW={'full'} 
+            height={"10vh"}
+            bg={'green.800'}
+            position={'relative'}
+            padding={0}
+        >
+            <Flex
+                justifyContent={'center'}
+                alignItems={'center'}
+                height={'100%'}
+            >
+                <Text
+                    color={"green.100"}
+                    fontSize={{ base: "sm", sm: "md", lg: "lg" }}
+                >
+                    © 2023 All rights reserved.
+                </Text>
+            </Flex>
+        </Container>
+    )
+}
