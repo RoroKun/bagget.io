91c0151b8bd49905e4c33078a45133d949bb2007
Alan <cortez.alan060@gmail.com>
2024-02-10 11:06:10 -0800

    made team section mobile responsive

--- app/sections/team.tsx
+++ app/sections/team.tsx
@@ -25,9 +25,22 @@ export default function Team() {
             paddingBottom={'5%'}
             id="Team"
         >
-            <Container maxW={"8xl"} bg={'green.800'} borderRadius={"100px 40px 100px 40px"}>
+            <Container 
+                maxW={"8xl"} 
+                bg={'green.800'} 
+                borderRadius={[
+                    "50px 35px",
+                    "100px 40px",
+                    "100px 40px",
+                ]}
+            >
                 <Container maxW={"7xl"} >
-                    <VStack spacing='24px' width={"full"} padding={'5% 0'}>
+                    <VStack 
+                        spacing='24px' 
+                        width={"full"} 
+                        paddingTop={'5%'} 
+                        paddingBottom={['25%', '15%', '15%']}
+                    >
                         <Title/>
                         <TeamMembers />
                     </VStack>
@@ -44,7 +57,8 @@ function Title() {
                 lineHeight={1.1}
                 fontWeight={600}
                 fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
-                >
+                textAlign={'center'}
+            >
                 <Text as={"span"} color={"green.100"}>
                     This is where{" "}
                 </Text>
@@ -61,9 +75,9 @@ function Title() {
                 </Text>
                 </Heading>
                 <Text
-                    maxW={'60%'}
+                    maxW={['90%', '80%', '60%']}
                     textAlign={'center'}
-                    fontSize={"xl"}
+                    fontSize={{ base: "sm", sm: "lg", lg: "xl" }}
                     color={'green.100'}
                 >
                     We are Cal State Fullerton CS majors looking to create custom solutions for this ecosystem 
@@ -75,12 +89,12 @@ function Title() {
 
 function TeamMembers() {
     return (
-        <SimpleGrid columns={2} spacing={20}>
+        <SimpleGrid columns={[1, 1, 2]} spacing={20}>
             {teamData.map((member, id) => 
                 <Box
                     position={'relative'}
-                    height={'475px'}
-                    width={'400px'}
+                    height={['275px', '375px', '375px']}
+                    width={['250px', '350px', '350px']}
                     key={id}
                 >
                     <Image
@@ -104,30 +118,31 @@ function TeamMembers() {
                         zIndex={4}
                         color={'green.100'}
                     >
-                        <VStack color={'green.800'} padding={'20px 30px'}>
+                        <VStack color={'green.800'} padding={['10px 15px','20px 30px','20px 30px']}>
                             <VStack
-                                _after={{
-                                    content: "''",
-                                    width: '80%',
-                                    height: '2px',
-                                    position: 'absolute',
-                                    top: '42%',
-                                    left: '50%',
-                                    transform: 'translateX(-50%)',
-                                    bg: 'green.800',
-                                    borderRadius: '20px',
-                                    zIndex: -1,
-                                }}
+                                
                                 paddingBottom={'10px'}
                             >
                                 <Heading
-                                    fontSize={'2xl'}
+                                    fontSize={['lg','xl','2xl']}
                                 >
                                     {member.name}
                                 </Heading>
                                 <Text
                                     position={'relative'}
-                                    fontSize={'xs'}
+                                    fontSize={['10px','xs','xs']}
+                                    _after={{
+                                        content: "''",
+                                        width: ['150px','200px','200px'],
+                                        height: '2px',
+                                        position: 'absolute',
+                                        bottom: '-70%',
+                                        left: '50%',
+                                        transform: 'translateX(-50%)',
+                                        bg: 'green.800',
+                                        borderRadius: '20px',
+                                        zIndex: -1,
+                                    }}
                                 >
                                     {member.title}
                                 </Text>
@@ -136,6 +151,33 @@ function TeamMembers() {
                                 justifyContent={'flex-start'}
                                 alignItems={'flex-start'}
                             >
+                                <RedirectLink
+                                    link={`mailto: ${member.email}`}
+                                    title={`Sends an email to ${member.name} in a new tab`}
+                                >
+                                    <HStack
+                                        height={['20px','40px','40px']}
+                                        justifyContent={'center'}
+                                        alignItems={'center'}
+                                        gap={'10px'}
+                                    >
+                                        <Box
+                                            width={['20px','30px','30px']}
+                                            height={'30px'}
+                                        >
+                                            <Email
+                                                w={'100%'}
+                                                h={'100%'}
+                                            />
+                                        </Box>
+                                        <Text
+                                            fontSize={{ base: "xs", sm: "sm", lg: "md" }}
+                                        >
+                                            {member.email}
+                                        </Text>
+                                    </HStack>
+                                </RedirectLink>
+                                
                                 <RedirectLink
                                     link={`https://www.linkedin.com/in/${member.linkedIn}/`}
                                     title={`Opens ${member.name}'s LinkedIn profile in a new tab`}
@@ -147,7 +189,7 @@ function TeamMembers() {
                                         gap={'10px'}
                                     >
                                             <Box
-                                                width={'30px'}
+                                                width={['20px','30px','30px']}
                                                 height={'30px'}
                                             >
                                                 <LinkedIn
@@ -155,35 +197,14 @@ function TeamMembers() {
                                                     h={'100%'}
                                                 />
                                             </Box>
-                                            <Text>
+                                            <Text
+                                                fontSize={{ base: "xs", sm: "sm", lg: "md" }}
+                                            >
                                                 {member.linkedIn}
                                             </Text>
                                     </HStack>
                                 </RedirectLink>
-                                <RedirectLink
-                                    link={`mailto: ${member.email}`}
-                                    title={`Sends an email to ${member.name} in a new tab`}
-                                >
-                                    <HStack
-                                        height={'40px'}
-                                        justifyContent={'center'}
-                                        alignItems={'center'}
-                                        gap={'10px'}
-                                    >
-                                        <Box
-                                            width={'30px'}
-                                            height={'30px'}
-                                        >
-                                            <Email
-                                                w={'100%'}
-                                                h={'100%'}
-                                            />
-                                        </Box>
-                                        <Text>
-                                            {member.email}
-                                        </Text>
-                                    </HStack>
-                                </RedirectLink>
+                                
                             </VStack>
                         </VStack>
                     </Box>
