4416d53d22740aaedd2e9e3c2bc7d2ec67ff87ef
Alan <cortez.alan060@gmail.com>
2024-02-09 17:40:46 -0800

    abstracted svgs into component

---
+++ app/components/svgs.tsx
@@ -0,0 +1,20 @@
+import { Icon, IconProps } from "@chakra-ui/react"
+
+export const Blob = (props: IconProps) => {
+    return (
+        <Icon
+            width={'100%'}
+            viewBox="0 0 755 588"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M62.4997 111C153.874 14.9541 269.09 149.681 401 136.5C535.99 123.012 687.771 -86.1759 735 41C766.42 125.609 639.615 167.031 651.5 256.5C661.589 332.457 754.379 346.876 754.5 423.5C754.705 554.052 580.073 617.112 456.5 575C378.34 548.364 385.686 454.123 309 423.5C228.891 391.51 162.957 480.054 88.4997 436.5C-21.5724 372.114 -25.3958 203.39 62.4997 111Z"
+                fill="currentColor"
+            />
+        </Icon>
+    )
+}
