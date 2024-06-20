ce439c21d42a8e9bd7b9b816d4ab0f273e2cb9fd
Alan <cortez.alan060@gmail.com>
2024-06-19 18:44:53 -0700

    update challenge interface to help with styles

--- data/challenges.ts
+++ data/challenges.ts
@@ -1,4 +1,5 @@
 export interface challenge {
+    stakeholder: string;
     title: string;
     blurb: string[];
     image: string;
@@ -7,16 +8,18 @@ export interface challenge {
 
 export const challengesData: challenge[] = [
     {
-        title: "Food Banks struggle to...",
+        stakeholder: "Food Banks",
+        title: "struggle to...",
         blurb: [
             "Collect and understand data to better serve the community",
             "Heavy reliance on understaffed personnel to communicate with partner agencies, resulting in long lead times & lost relationships"
         ],
         image: "/fb-challenge.webp",
-        bgColor: 'green.100'
+        bgColor: 'linear(to-tr, green.100, green.200)'
     },
     {
-        title: "Food Pantries are faced with...",
+        stakeholder: "Food Pantries",
+        title: "are faced with...",
         blurb: [
             "Difficulty in getting and retaining volunteers",
             "Maintaining engagement with their donor base",
@@ -24,15 +27,16 @@ export const challengesData: challenge[] = [
             "Replacing phone calls/emails/paperwork with one intuitive digital experience",
         ],
         image: "/fp-challenge.webp",
-        bgColor: 'green.200'
+        bgColor: 'linear(to-bl, green.100, green.200)'
     },
     {
-        title: "The Community is left helpless due to...",
+        stakeholder: "The Community",
+        title: "is left helpless due to...",
         blurb: [
             "The lack of up-to-date information about their local pantries and food agencies",
             "Inevitably long & haphazard lines at distribution locations due to poor client scheduling and slow intakes",
             "Vastly varied check-in procedures pantry to pantry",
         ],image: "/community-challenge.webp",
-        bgColor: 'green.100'
+        bgColor: 'linear(to-tl, green.100, green.200)'
     }
 ]
