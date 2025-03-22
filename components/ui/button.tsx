d142910b1981b7a05014cca42c2945944c76e09c
Alan <cortez.alan060@gmail.com>
2025-03-22 15:19:06 -0700

    add shadcn components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,18 +1,79 @@
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+import { Text } from "@/components/utility/typography";
 import { Redirect } from "../utility/link";
-import { twMerge } from "tailwind-merge";
 
-export function Button({className, variant="primary", link, children}: {className?: string; variant?: string; link: string; children: React.ReactNode}) {
+
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+        outline:
+          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+        ghost:
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        icon: "size-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const Comp = asChild ? Slot : "button"
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+}
+
+
+function BetterButton({className, variant="primary", link, children}: {className?: string; variant?: string; link: string; children: React.ReactNode}) {
 
   
-    const baseStyle = "w-full px-10 py-3 rounded-2xl"
+    const baseStyle = "w-full px-6 py-4 rounded-2xl font-semibold cursor-pointer"
     const primary = "bg-gradient-to-r from-emerald-950 to-green-900 hover:from-emerald-800 hover:to-green-800"
-    const secondary = "bg-gradient-to-r from-lime-50 to-lime-100 hover:from-emerald-800 hover:to-green-800"
+    const secondary = "bg-gradient-to-r from-lime-50 to-lime-100 hover:from-lime-100 hover:to-lime-200 text-gray-950 weight-semibold"
 
     const variantStyle = variant === "primary" ? primary : secondary
 
     return (
       <Redirect redirect={link}>
-        <button className={twMerge(baseStyle, variantStyle, className)}>{children}</button>
+        <Button className={cn(baseStyle, variantStyle, className)}>{children}</Button>
       </Redirect>
     )
   }
+
+export { Button, BetterButton, buttonVariants }
