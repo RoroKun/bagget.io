ba1b4c6018cf27ff743d1ffe5076a7718f22ac75
Alan <cortez.alan060@gmail.com>
2025-03-23 16:38:34 -0700

    first version of the our story page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import Hero from "@/components/sections/globalHero";
+import Image from "next/image";
+import { Heading, Text, Highlight } from "@/components/utility/typography";
+import { newsData } from "@/data/news";
+import { BetterButton } from "@/components/ui/button";
 
 export default function NewsPage() {
     return (
         <main>
-            test
+            <Hero image={{url: "/news-hero.webp", alt: "picture of newspapers"}}>
+                See what <Highlight>we've</Highlight> been up to.
+            </Hero>
+            <News/>
         </main>
+    )
+}
+
+function News() {
+    return (
+        <section className="px-[15%] py-[2%] flex flex-col gap-10 justify-center items-center bg-gradient-to-b from-green-950 to-emerald-900 text-center text-gray-50">
+            <div className="flex flex-col gap-14">
+                {newsData.map((news, i) => (
+                    <div key={`${news.title}-id-${i}`} className="relative flex flex-col justify-center items-center gap-6 bg-green-950 shadow-lg rounded-full px-20 py-28 overflow-hidden">
+                        <div className="flex flex-col gap-5 relate z-10">
+                            <Heading size="big">{news.title}</Heading>
+                            <Text>{news.location ? `${news.location} · ${news.date}` : news.date} </Text>
+                        </div>
+                        <div className="flex flex-col gap-8 w-1/2 justify-center items-center relate z-10">
+                            <Text>{news.blurb}</Text>
+                            {news.link && <BetterButton link={news.link} newPage variant="secondary"><Text>Read More</Text></BetterButton>}
+                        </div>
+                        <div className="absolute z-0 w-full h-full">
+                            <Image 
+                                src={news.image.url}
+                                alt={news.image.alt}
+                                width={1920}
+                                height={1080}
+                            />
+                            <div className="absolute inset-0 bg-black opacity-90"></div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 }
